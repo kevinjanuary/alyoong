13f36c320a99b3f429df3309c5f28116c24ea866
Kevin Januari <128971428+kevinjanuary@users.noreply.github.com>
2025-01-12 19:42:06 +0700

    fix: categories home

--- app/(root)/_components/categories.tsx
+++ app/(root)/_components/categories.tsx
@@ -1,37 +1,58 @@
-import { Monitor, MemoryStick, Keyboard, Mouse } from "lucide-react"
+import { db } from "@/lib/prismadb"
+import {
+  Gamepad2Icon,
+  Keyboard,
+  LucideIcon,
+  MemoryStick,
+  Monitor,
+  Mouse,
+} from "lucide-react"
 import Link from "next/link"
 
-const Categories = () => {
+const CategoryIcons: Record<string, LucideIcon> = {
+  Monitor: Monitor,
+  "Graphic Card": MemoryStick,
+  Keyboard: Keyboard,
+  Mouse: Mouse,
+}
+
+const Categories = async () => {
+  const categories = await db.product.groupBy({
+    by: ["category"],
+    _count: {
+      id: true,
+    },
+    orderBy: {
+      _count: {
+        id: "desc",
+      },
+    },
+  })
+
+  const max8Categories = categories.slice(0, 8)
+
   return (
     <div className="mt-4 grid grid-cols-4 gap-4 max-w-5xl w-full">
-      <Link href="/">
-        <div className="border rounded-lg bg-white p-6 flex flex-col shadow-sm">
-          <Monitor />
-          <span>Monitor</span>
-          <span>{Math.floor(Math.random() * 201) + 50} products</span>
-        </div>
-      </Link>
-      <Link href="/">
-        <div className="border rounded-lg bg-white p-6 flex flex-col shadow-sm">
-          <MemoryStick />
-          <span>Graphic Card</span>
-          <span>{Math.floor(Math.random() * 201) + 50} products</span>
-        </div>
-      </Link>
-      <Link href="/">
-        <div className="border rounded-lg bg-white p-6 flex flex-col shadow-sm">
-          <Keyboard />
-          <span>Keyboard</span>
-          <span>{Math.floor(Math.random() * 201) + 50} products</span>
-        </div>
-      </Link>
-      <Link href="/">
-        <div className="border rounded-lg bg-white p-6 flex flex-col shadow-sm">
-          <Mouse />
-          <span>Mouse</span>
-          <span>{Math.floor(Math.random() * 201) + 50} products</span>
-        </div>
-      </Link>
+      {max8Categories.map((category) => {
+        const Icon = CategoryIcons[category.category] || Gamepad2Icon
+
+        return (
+          <Link
+            key={category.category}
+            href={`/products?category=${category.category}`}
+          >
+            <div className="border rounded-lg hover:bg-secondary transition-colors p-6 flex items-center gap-4 shadow-md text-sm">
+              <Icon className="w-8 h-8" />
+              <div className="flex flex-col">
+                <span className="font-semibold">{category.category}</span>
+                <span className="text-gray-500">
+                  {category._count.id} products
+                </span>
+              </div>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
